436ae021af2cb4e6a43ba2ae4b8c732c33d65d95
FadhilM <mudzakifadhil@gmail.com>
2025-10-07 14:55:07 +0700

    first commit

---
+++ prak1.py
@@ -0,0 +1,12 @@
+percentage = float(input('masukkan persentase nilai siswa: '))
+
+if percentage >= 90:
+    print("excelent performance")
+elif percentage >= 80:
+    print("very good performance")
+elif percentage >= 70:
+    print("good performance")
+elif percentage >= 60:
+    print("average performance")
+else:
+    print("needs improvement")
